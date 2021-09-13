17a2364a08962deff76c009ee9449f2d91991cd2
Merk68 <marko.mark2001@gmail.com>
2021-09-13 10:53:21 +0200

    Kiindulás.

--- Jackie/Program.cs
+++ Jackie/Program.cs
@@ -3,12 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Jackie
 {
   class Program
   {
     static void Main(string[] args)
+    {
+      ElsoFeladat();
+      MasodikFeladat();
+      HarmadikFeladat();
+      NegyedikFeladat();
+      OtodikFeladat();
+      HatodikFeladat();
+      Console.ReadKey();
+    }
+
+    private static void HatodikFeladat()
+    {   
+    }
+
+    private static void OtodikFeladat()
+    {  
+    }
+
+    private static void NegyedikFeladat()
+    {
+    }
+
+    private static void HarmadikFeladat()
+    {
+    }
+
+    private static void MasodikFeladat()
+    {
+    }
+
+    private static void ElsoFeladat()
     {
     }
   }
